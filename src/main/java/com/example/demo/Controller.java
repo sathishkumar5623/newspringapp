3dc65053ddcae8b1a983561a08d183f945bdc897
sathishkumar <130451333+sathishkumar5623@users.noreply.github.com>
2023-05-04 17:32:48 +0530

    Update Controller.java

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -9,6 +9,15 @@ public class Controller {
 	@GetMapping("/check")
 	private String data(){
 		return "AWS CICD was sucessfully created\n hello";
+		
 	}
+		@GetMapping("/getUserByAddress/{address}")
+	public List<User> findUserByAddress(@PathVariable String address) {
+		return service.getUserbyAddress(address);
+	}
+		
+		
+	
+	
 
 }
